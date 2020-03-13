5e72a9e4efece42495da4ac7cee544c4e083c915
Wouter Vroege <wouter@woutervroege.nl>
2020-03-13 15:02:57 +0100

    added select
    Signed-off-by: Wouter Vroege <wouter@woutervroege.nl>

---
+++ packages/select/lib/select.js
@@ -0,0 +1,196 @@
+import { Properties } from 'html-element-property-mixins';
+import { StringConverter, NumberConverter, BooleanConverter } from 'html-element-property-mixins/src/utils/attribute-converters';
+import { html, render as litRender} from 'lit-html';
+export { html } from 'lit-html';
+
+export class HTMLSelectElement extends Properties(HTMLElement) {
+  
+  static get properties() {
+
+    return {
+      
+      accessKey: {
+        observe: true,
+        DOM: true,
+        reflect: true,
+        fromAttributeConverter: StringConverter.fromAttribute,
+        toAttributeConverter: StringConverter.toAttribute,
+      },
+
+      autofocus: {
+        observe: true,
+        DOM: true,
+        reflect: true,
+        fromAttributeConverter: BooleanConverter.fromAttribute,
+        toAttributeConverter: BooleanConverter.toAttribute,
+      },
+
+      disabled: {
+        observe: true,
+        DOM: true,
+        reflect: true,
+        fromAttributeConverter: BooleanConverter.fromAttribute,
+        toAttributeConverter: BooleanConverter.toAttribute,
+      },
+
+      tabIndex: {
+        observe: true,
+        DOM: true,
+        reflect: true,
+        fromAttributeConverter: NumberConverter.fromAttribute,
+        toAttributeConverter: NumberConverter.toAttribute,
+      },
+
+      length: {
+        observe: true,
+        DOM: true,
+        reflect: true,
+        fromAttributeConverter: NumberConverter.fromAttribute,
+        toAttributeConverter: NumberConverter.toAttribute,
+      },
+
+      multiple: {
+        observe: true,
+        DOM: true,
+        reflect: true,
+        fromAttributeConverter: BooleanConverter.fromAttribute,
+        toAttributeConverter: BooleanConverter.toAttribute,
+      },
+
+      required: {
+        observe: true,
+        DOM: true,
+        reflect: true,
+        fromAttributeConverter: BooleanConverter.fromAttribute,
+        toAttributeConverter: BooleanConverter.toAttribute,
+      },
+
+      selectedIndex: {
+        observe: true,
+        DOM: true,
+        reflect: true,
+        fromAttributeConverter: NumberConverter.fromAttribute,
+        toAttributeConverter: NumberConverter.toAttribute,
+      },
+
+      size: {
+        observe: true,
+        DOM: true,
+        reflect: true,
+        fromAttributeConverter: NumberConverter.fromAttribute,
+        toAttributeConverter: NumberConverter.toAttribute,
+      },
+
+    }
+
+  }
+
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open', delegatesFocus: true});
+    this.tabIndex = 0;
+    this.accessKey = '';
+    this.selectedIndex = 0;
+    this.render();
+    this.__initFocusDelegation();
+    this.shadowRoot.addEventListener('slotchange', this._handleSlotChange.bind(this));
+  }
+
+  propertyChangedCallback(propName, oldValue, newValue) {
+    super.propertyChangedCallback(propName, oldValue, newValue);
+    this.render();
+  }
+
+  get styles() {
+    return html`
+      <style>
+        :host { outline: none }
+      </style>
+    `
+  }
+
+  get template() {
+    return html`
+      ${this.styles}
+      <select
+        .accessKey="${this.accessKey}"
+        ?autofocus="${this.autofocus}"
+        ?disabled="${this.disabled}"
+        .length="${this.length}"
+        ?multiple="${this.multiple}"
+        .selectedIndex="${this.selectedIndex}"
+        .size="${this.size}"
+        .tabIndex="${this.tabIndex}"
+        ?required="${this.required}"
+        @input="${(e) => this.selectedIndex = e.target.selectedIndex}"
+        @change="${(e) => this.selectedIndex = e.target.selectedIndex}"
+      ></select>
+      <slot hidden></slot>
+    `
+  }
+
+  render() {
+    window.cancelAnimationFrame(this._renderDebouncer);
+    this._renderDebouncer = window.requestAnimationFrame(() => {
+      litRender(this.template, this.shadowRoot, {eventContext: this})  
+    })
+  }
+
+  get labels() {
+    return this.$element.labels;
+  }
+
+  get options() {
+    return this.$element.options;
+  }
+
+  get selectedOptions() {
+    return this.$element.selectedOptions;
+  }
+
+  get type() {
+    return this.$element.type;
+  }
+
+  get value() {
+    return this.$element.value;
+  }
+
+  get willValidate() {
+    return this.$element.willValidate();
+  }
+
+  get validationMessage() {
+    return this.$element.validationMessage();
+  }
+
+  get validity() {
+    return this.$element.validity();
+  }
+
+  checkValidity()	{
+    return this.$element.checkValidity();
+  }
+
+  reportValidity() {
+    return this.$element.reportValidity();
+  }
+
+  setCustomValidity(val) {
+    this.$element.setCustomValidity(val);
+  }
+
+  get $element() {
+    return this.shadowRoot.querySelector('select');
+  }
+
+  _handleSlotChange(e) {
+    this.$element.innerHTML = this.innerHTML;
+  }
+
+  __initFocusDelegation() {
+    if(this.shadowRoot.delegatesFocus) return;
+    this.addEventListener('focus', () => this.$element.focus());
+  }
+
+}
